9f90f1e58a6377cb25604d414b6a942fa642c3c6
TienLM <tienlm1509@gmail.com>
2019-07-13 03:45:33 +0700

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ "$1" = 'puma' ]; then
 
-  # if [ -z "${SECRET_KEY_BASE}" ]; then
-  #   echo 'WARNING: Environment variable SECRET_KEY_BASE has not been set - Setting a random SECRET_KEY_BASE'
-  #   export SECRET_KEY_BASE="$(bundle exec rake secret)"
-  # fi
+  if [ -z "${SECRET_KEY_BASE}" ]; then
+    echo 'WARNING: Environment variable SECRET_KEY_BASE has not been set - Setting a random SECRET_KEY_BASE'
+    export SECRET_KEY_BASE="$(bundle exec rake secret)"
+  fi
 
   # Ensure db:migrations have run if upgraded
   bundle exec rake db:migrate
