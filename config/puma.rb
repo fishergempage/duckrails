c4b1816bab5d4ba1accac6e529b6731c413129d6
TienLM <tienlm1509@gmail.com>
2019-07-13 03:29:03 +0700

    update

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,12 @@
 workers Integer(ENV["PUMA_WORKERS"] || 2)
-threads_count = Integer(ENV["PUMA_LISTEN_PORT"] || 80)
+threads_count = Integer(ENV["RAILS_MAX_THREADS"] || 5)
 threads threads_count, threads_count
 
 preload_app!
+
+rackup DefaultRackup
+port ENV["PORT"] || 3000
+environment ENV["RACK_ENV"] || "development"
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
